520973f64051c656371292db595217b90cbc69f9
David Cole <david@nubuilder.com>
2016-05-31 11:54:26 +0930

    Refactored nudragform code.

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ require_once('nucommon.php'); ?>
 			nuGetForm(p.form_id, p.record_id);
 
             if(p.record_id == '-2')
-                bindDragEvents();
+                nuBindDragEvents();
 
 		}
 		
