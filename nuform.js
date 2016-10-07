6cc61c7a9cef7977e452de0427d2b053ae5625f9
steven-copley <steven@nubuilder.com>
2016-10-07 11:41:49 +1030

    fixed label not displaying

--- nuform.js
+++ nuform.js
@@ -330,7 +330,8 @@ function nuINPUT(w, i, l, p, prop){
 	var id			= p + prop.objects[i].id;
 	var ef			= p + 'nuRecordHolder';                 //-- Edit Form Id
 	var ty			= 'textarea';
-	
+	var vis			= prop.objects[i].display == 0 ? 'hidden' : 'visible';
+
 	if(prop.objects[i].type != 'textarea'){         		//-- Input Object
 		ty			= 'input';
 	}
@@ -436,6 +437,7 @@ function nuINPUT(w, i, l, p, prop){
 		.attr('onblur', 'nuLookupBlur(event)')
 		.attr('onfocus', 'nuLookupFocus(event)')
 		.attr('onchange', "$('#' + id).removeClass('nuValidate')")
+		.css('visibility', vis)
 		.addClass('nuLookupCode');
 		
 		w.objects[i].values[0][0]	= p + w.objects[i].values[0][0];
@@ -461,9 +463,10 @@ function nuINPUT(w, i, l, p, prop){
 		.attr('data-nu-object-id', w.objects[i].object_id)
 		.attr('data-nu-target', target)
 		.attr('data-nu-subform-sort', 1)
+		.attr('onclick', 'nuBuildLookup(this,[])')
 		.addClass('nuLookupButton')
 		.html('<img border="0" src="lookup.png" class="nuLookupImg">')
-		.attr('onclick', 'nuBuildLookup(this,[])');
+		.css('visibility', vis);
 
 		id = p + prop.objects[i].id + 'description';
 		var inp = document.createElement('input');
@@ -474,8 +477,8 @@ function nuINPUT(w, i, l, p, prop){
 		$('#' + id).css({'top'		: Number(prop.objects[i].top),
 						'left'		: Number(prop.objects[i].left) + Number(prop.objects[i].width) + 21,
 						'width'		: prop.objects[i].description_width,
-						'visibility'	: prop.objects[i].description_width == 0 ? 'hidden' : 'visible',
-						'height'		: Number(prop.objects[i].height)
+						'visibility': prop.objects[i].description_width == 0 || prop.objects[i].display == 0 ? 'hidden' : 'visible',
+						'height'	: Number(prop.objects[i].height)
 		})
 		.attr('tabindex','-1')
 		.addClass('nuLookupDescription')
@@ -922,7 +925,6 @@ function nuLabel(w, i, p, prop){
 	.html(l)
 	.attr('ondblclick','nuBuildPopup("nuobject", "' + prop.objects[i].object_id + '")');
 
-	if(true){	console.log(id, '==',nuTranslate(prop.objects[i].label));}
 
 }
 
