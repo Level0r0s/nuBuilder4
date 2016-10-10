8e97b3939abfc42397d687d00e486a5a537e270e
Jarad Anderson <jarad@nubuilder.com>
2016-10-11 09:24:23 +1030

    added form object class

---
+++ nuformobject.js
@@ -0,0 +1,19 @@
+
+class nuFormoObject {
+	
+	constructor() {
+		this.nuBreadCrumb[] = [];
+	}
+
+	function addBC() {
+		this.nuBreadCrumb[] = new nuBreadCrumb;
+	}
+
+	function getBC(n) {
+		return this.BC[n];
+	}
+}
+
+class nuBreadCrumb {
+	
+}
