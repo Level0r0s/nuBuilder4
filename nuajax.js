c487ec26d0e1e247e9ff879121339aae4ae6de1d
Adam <adam@nubuilder.com>
2017-03-03 11:57:24 +1030

    Added opener. Index.php is expecting it and logs you out otherwise.

--- nuajax.js
+++ nuajax.js
@@ -425,8 +425,9 @@ function nuOpenNewBrowserTab(c, f, r, filter){
 
 		nuOpenerAppend('type', c);
 		
-		var open 		= window.nuOPENER.length - 1;
-		var u			= window.location.href + '?i=' + open;
+	    var len   	= window.nuOPENER.length - 1;
+		var id 	    = window.nuOPENER[window.nuOPENER.length - 1].id;
+		var u		= window.location.href + '?i=' + len + '&opener=' + id ;
 
 		window.open(u);
 
