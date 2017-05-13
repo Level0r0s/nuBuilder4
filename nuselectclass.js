55fccfaba0b469a99101d524ba379ab77ef0f396
steven-copley <freddy77>
2017-05-14 05:12:08 +0930

    .

--- nuselectclass.js
+++ nuselectclass.js
@@ -3,8 +3,9 @@ class nuSelectObject{
 	
 	constructor() {
 		
-		this.boxes			= [];
-		this.boxID			= '';
+		this.boxes		= [];
+		this.boxID		= '';
+		this.rows		= 0;
 		
 	}
 	
@@ -14,10 +15,11 @@ class nuSelectObject{
 		var s			= nuFORM.tableSchema
 		var n			= s[t].names;
 		var f			= n.length;
-		var u			= nuID();
-		this.boxID		= 'box' + u;
-		this.scrollID	= 'scroll' + u;
-		var h			= this.boxHeader();
+		var i			= nuID();
+		this.boxID		= 'box' + i;
+		this.scrollID	= 'scroll' + i;
+		
+		this.boxHeader();
 		
 		var box			= document.createElement('div');
 
@@ -25,76 +27,170 @@ class nuSelectObject{
 		
 		$('body').append(box);
 		$('#' + this.boxID).css({
-			'width'        		: 900,
+			'width'        		: 914,
 			'height'       		: 200,
-			'top'				: 20,
+			'top'				: 190,
 			'left'				: 20,
 			'border'			: 'solid grey 1px',
 			'overflow'			: 'hidden',
 			'padding-top'		: '5px',
-			'line-height'		: '25px',
+			'line-height'		: '20px',
 		})
-		.addClass('nuBoxHeader')
-		.html(h);
+		.addClass('nuBoxHeader');
 
 		var scroll	= document.createElement('div');
 
-		scroll.setAttribute('id', 'scroll' + u);
+		scroll.setAttribute('id', 'scroll' + i);
 		
 		$('#' + this.boxID).append(scroll);
 		$('#' + scroll.id).css({
-			'width'        		: 900,
+			'width'        		: 914,
 			'height'       		: 150,
 			'top'				: 50,
 			'left'				: 0,
 			'overflow'			: 'scroll',
 			'overflow-x'		: 'hidden',
-			'line-height'		: '30x',
+			'line-height'		: 30,
 		})
 		.addClass('nuBoxHeader');
+
+
+
+
+
+
+
+
+
+
+
 		
-		for(var i = 0 ; i < f ; i++){
-			this.boxRow(i);
+		var x = document.createElement('div');
+		
+		x.setAttribute('id', 'nuBoxClose' + this.boxID);
+		
+		$('#' + this.boxID).append(x);
+		
+		$('#' + x.id).css({
+			'width'				: 20,
+			'height'			: 20,
+			'top'				: 2,
+			'left'				: 2,
+			'position'			: 'absolute',
+			'text-align'    	: 'center'
+		})
+		.attr('onclick', '$("#nuSearchList").remove();$("#nuModal").remove();')
+		.html('<span style="font-size:14px"><b>X</b></span>')
+		.addClass('nuSearchListClose');
+		
+
+		
+		var s = document.createElement('div');
+		
+		s.setAttribute('id', 'nu_more_less' + this.boxID);
+		
+		$('#' + this.boxID).append(s);
+		
+		$('#' + s.id).css({
+			'width'				: 15,
+			'height'			: 15,
+			'top'				: 5,
+			'left'				: 285,
+			'position'			: 'absolute',
+			'text-align'    	: 'center'
+		})
+		.attr('onclick', '$("#nuSearchList").remove();$("#nuModal").remove();')
+		.html('<img id="numl' + this.boxID + '" src="nu_less.png" width="10px" height="10px" style="padding:3px">')
+		.addClass('nuSearchListClose');
+
+		this.boxTitles();
+		
+
+		for(this.rows = 0 ; this.rows < f ; this.rows++){
+			this.boxRow(this.rows);
 		}
-		$('#' + this.scrollID).append('&nbsp;<span>+</span>');
 
-		
 	}
 
-	boxHeader(){
+	boxTitles(){
 		
-		var h	= nuSpaces(3) + 'Table<br>' + nuSpaces(3) + 'Field' + nuSpaces(30) + 'Alias' + nuSpaces(30) + 'Where' + nuSpaces(30) + 'OB' + nuSpaces(2) + 'GP' + nuSpaces(2) + 'Having';
+		var t	= 30;
 		
-		return h;
-		
-	}
-	
-
-	boxRow(i){
-		
-		this.boxColumn('field', i, 200, '');
-		this.boxColumn('alias', i, 200, '');
-		this.boxColumn('where', i, 200, '');
-		this.boxColumn('ob', i, 40, '');
-		this.boxColumn('gb', i, 40, '');
-		this.boxColumn('having', i, 200, '');
+		this.boxTitle('Field', 	t, 130);
+		this.boxTitle('Alias', 	t, 335);
+		this.boxTitle('Where', 	t, 470);
+		this.boxTitle('OB', 	t, 618);
+		this.boxTitle('GB', 	t, 660);
+		this.boxTitle('Having',	t, 765);
 		
 	}
 	
 
-	boxColumn(c, u, w, v){
+	boxTitle(v, t, l){
+
+		var col	= document.createElement('span');
+
+		col.setAttribute('id', String(v).replaceAll(' ', '_') + '_' + this.boxID);
+		
+		$('#' + this.boxID).append(col);
+		
+		$('#' + col.id)
+		.css({
+			'position'	:  'absolute',
+			'top'		:  t,
+			'left'		:  l,
+		})
+		.html(v);
+		
+		
+	}
+	
+	boxRow(i){										//--------------name row left width value
+		
+		this.boxColumn('field', i, 2,  300, '');
+		this.boxColumn('alias', i, 304, 100, '');
+		this.boxColumn('where', i, 406, 200, '');
+		this.boxColumn('ob', 	i, 608, 40,  '');
+		this.boxColumn('gb', 	i, 650, 40,  '');
+		this.boxColumn('having',i, 692, 200, '');
+		
+	}
+	
+
+	boxHeader(c, t, l, w, v){
 
 		var col	= document.createElement('input');
 
-		col.setAttribute('id', c + u);
+		col.setAttribute('id', c + '_' + this.boxID);
+		
+		$('#' + this.boxID).append(col);
+		
+		$('#' + col.id)
+		.css({
+			'position'	:  'absolute',
+			'width'		:  w,
+			'top'		:  t,
+			'left'		:  l,
+		});
+		
+	}
+	
+
+	boxColumn(c, t, l, w, v){
+
+		var col	= document.createElement('input');
+
+		col.setAttribute('id', c + '_' + t + '_' + this.boxID);
 		
 		$('#' + this.scrollID).append(col);
 		
-		$('#' + col.id).css('width',  Number(w));
-		
-		if(c == 'having'){
-			$('#' + this.scrollID).append('<br>');
-		}
+		$('#' + col.id)
+		.css({
+			'position'	:  'absolute',
+			'width'		:  w,
+			'top'		:  t * 20,
+			'left'		:  l,
+		});
 		
 	}
 	
