de6adf065840e95d1997f0ad9635fdfc224b6757
David Cole <david@nubuilder.com>
2017-07-12 11:17:56 +0930

    Wrote tests to test login, and adding a new table while logging in, with core code erroring if schema is incorrect.

--- config.php
+++ config.php
@@ -1,17 +1,19 @@
 <?php
 
-    if(strpos($_SERVER['REQUEST_URI'], basename(__FILE__)) !== false){
-        header('HTTP/1.0 404 Not Found', true, 404);
-        die();
-    } else {
-        $nuConfigDBHost                 = "127.0.0.1";
-        $nuConfigDBName                 = "nubuilder4";
-        $nuConfigDBUser                 = "root";
-        $nuConfigDBPassword             = "";
-        $nuConfigDBGlobeadminUsername   = "globeadmin";     //-- globeadmin username
-        $nuConfigDBGlobeadminPassword   = "nu";             //-- globeadmin password
-        $nuConfigTitle                  = "nuBuilder 4";
-        $nuConfigIsDemo                 = false;
+    $nuConfigDBHost                 = "127.0.0.1";
+    $nuConfigDBName                 = "nubuilder4";
+    $nuConfigDBUser                 = "root";
+    $nuConfigDBPassword             = "";
+    $nuConfigDBGlobeadminUsername   = "globeadmin";     //-- globeadmin username
+    $nuConfigDBGlobeadminPassword   = "nu";             //-- globeadmin password
+    $nuConfigTitle                  = "nuBuilder 4";
+    $nuConfigIsDemo                 = false;
+
+    if(array_key_exists('REQUEST_URI', $_SERVER)){
+        if(strpos($_SERVER['REQUEST_URI'], basename(__FILE__)) !== false){
+            header('HTTP/1.0 404 Not Found', true, 404);
+            die();
+        }
     }
 
 ?>
